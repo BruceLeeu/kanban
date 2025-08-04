fcc37395a74af171c3e218e6f9ad399e7d3cca06
Louis La Grange <louis@louislagrange.co.za>
2025-08-04 10:34:21 +0200

    Implement basic dragdrop functionality (borrowed from cdkDragDrop)

---
+++ src/app/components/task-card/task-card.ts
@@ -0,0 +1,20 @@
+import { Component, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+
+export interface Task {
+  title: string;
+  content: string;
+  status: 'todo' | 'in-progress' | 'done';
+}
+
+@Component({
+  selector: 'app-task-card',
+  imports: [MatCardModule, MatChipsModule],
+  templateUrl: './task-card.html',
+  styleUrl: './task-card.scss',
+})
+export class TaskCard {
+  public title = input.required<string>();
+  public status = input<Task['status']>('todo');
+}
--- src/app/pages/kanban/kanban.ts
+++ src/app/pages/kanban/kanban.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Taskbar } from '../../components/taskbar/taskbar';
 import { MatCardModule } from '@angular/material/card';
+import { Task, TaskCard } from '../../components/task-card/task-card';
+import {
+  CdkDragDrop,
+  DragDropModule,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-kanban',
-  imports: [Taskbar, MatCardModule],
+  imports: [Taskbar, MatCardModule, TaskCard, DragDropModule],
   templateUrl: './kanban.html',
   styleUrl: './kanban.scss',
 })
-export class Kanban {}
+export class Kanban {
+  public todos = signal<Array<Task>>([
+    {
+      title: 'Mijn Taak Titel',
+      content: 'Dit moet er gebeuren',
+      status: 'todo',
+    },
+    {
+      title: 'Mijn Tweede Taak Titel',
+      content: 'Dit moet er ook nog gebeuren',
+      status: 'todo',
+    },
+  ]);
+  public inProgresses = signal<Array<Task>>([
+    {
+      title: 'DE Taak Titel',
+      content: 'Dit moet er gebeuren',
+      status: 'todo',
+    },
+    {
+      title: 'DE Tweede Taak Titel',
+      content: 'Dit moet er ook nog gebeuren',
+      status: 'todo',
+    },
+  ]);
+  public dones = signal<Array<Task>>([
+    {
+      title: 'EEN Taak Titel',
+      content: 'Dit moet er gebeuren',
+      status: 'todo',
+    },
+    {
+      title: 'EEN Tweede Taak Titel',
+      content: 'Dit moet er ook nog gebeuren',
+      status: 'todo',
+    },
+  ]);
+
+  drop(dragDropEvent: CdkDragDrop<Array<Task>>) {
+    if (dragDropEvent.previousContainer === dragDropEvent.container) {
+      moveItemInArray(
+        dragDropEvent.container.data,
+        dragDropEvent.previousIndex,
+        dragDropEvent.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        dragDropEvent.previousContainer.data,
+        dragDropEvent.container.data,
+        dragDropEvent.previousIndex,
+        dragDropEvent.currentIndex
+      );
+    }
+  }
+}
